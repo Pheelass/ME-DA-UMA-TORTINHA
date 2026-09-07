b47590c22a6d27721e5a78c7ac1e1cf6ad4accd1
Pheelass <phelagleetz@gmail.com>
2026-09-06 21:09:23 -0300

    atualizacao da pasta

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-if(true){
-	console.log('?')
-}
+let addContato = document.querySelector(".addContato");
+let conversas = document.getElementById("conversas")
+let grupos = document.getElementById("grupos");
+let encontros = document.getElementById("encontro");
+let telefone = document.getElementById("telefone");
+
+conversas.addEventListener("click", () => {
+	console.log('chat section clicked');
+});
+
+grupos.addEventListener("click", () => {
+	console.log('group section clicked')	
+});
+
+encontros.addEventListener("click", () => {
+	console.log('for you section clicked')	
+});
+
+
+telefone.addEventListener("click", () => {
+	console.log('call section clicked')	
+});
+
+addContato.addEventListener("click", async () => {
+	let DADOS = await fetch('/users');
+	let users = await DADOS.json();
+
+	console.log(users)
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("main.js loaded sucessfuly")
+})
